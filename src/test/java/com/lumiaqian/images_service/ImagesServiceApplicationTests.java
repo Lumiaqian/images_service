8f2a6c8918874f44bd6628fa18d2686ae536385f
lumiaqian <cao156@outlook.com>
2020-04-07 17:42:51 +0800

    删除测试用例

--- src/test/java/com/lumiaqian/images_service/ImagesServiceApplicationTests.java
+++ src/test/java/com/lumiaqian/images_service/ImagesServiceApplicationTests.java
@@ -1,6 +1,5 @@
-package com.lumiaqian.iamges_service;
+package com.lumiaqian.images_service;
 
-import com.lumiaqian.images_service.ImagesServiceApplication;
 import com.lumiaqian.images_service.Service.IpDirRepository;
 import com.lumiaqian.images_service.Utils.StringUtil;
 import org.junit.Test;
@@ -10,9 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.StringUtils;
-
-import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ImagesServiceApplication.class)
@@ -24,13 +20,13 @@ public class ImagesServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String ip = "120.236.153.209";
+		/*String ip = "120.236.153.209";
 		String pa = "lumia";
 		// pathRepository.save(path);
 		//logger.debug("第一次从MongoDB中取出的数据：" + pathRepository.findByIP(ip));
 		// pathRepository.update(path);
 		logger.info("第二次从MongoDB中取出的数据：" + ipDirRepository.findByIP(ip));
-		logger.info("UUID:"+ StringUtil.getRandomString(5));
+		logger.info("UUID:"+ StringUtil.getRandomString(5));*/
 
 	}
 
